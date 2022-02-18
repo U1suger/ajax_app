52c0557f0e90813f42228d2686171c2cdc437081
U1suger <u1.suger1201@gmail.com>
2022-02-18 10:48:51 +0900

    AjaxAppの準備

--- db/migrate/20210714023518_create_posts.rb
+++ db/migrate/20210714023518_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.text :content
-
+      t.boolean :checked
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_07_14_023518) do
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
